b75e2a5ee0229a8cdfa961c432964a1832b3183b
Anthony Edwards Jr <Anthony.edwardsjr@gmail.com>
2016-07-10 17:03:47 -0400

    inital state

---
+++ app/pages/page1/page1.js
@@ -0,0 +1,17 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/page1/page1.html'
+})
+export class Page1 {
+  constructor() {
+    this.items = [
+     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.png", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
+     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.png", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
+     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.png", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
+     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.png", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
+     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.png", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."}
+    ];
+  }
+}
