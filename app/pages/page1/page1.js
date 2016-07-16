4236952e8f67da73d068ea9382599bbc2ce578c0
Anthony Edwards Jr <Anthony.edwardsjr@gmail.com>
2016-07-16 14:44:47 -0400

    implmented request to server

--- app/pages/page1/page1.js
+++ app/pages/page1/page1.js
@@ -1,6 +1,8 @@
 import {Page, NavController} from 'ionic-angular';
 import {LocationDetailPage} from '../location-detail/location-detail';
 import {MapListPage} from '../map-list-page/map-list-page';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Page({
   templateUrl: 'build/pages/page1/page1.html'
@@ -8,20 +10,23 @@ import {MapListPage} from '../map-list-page/map-list-page';
 
 export class Page1{
   static get parameters() {
-    return [[NavController]];
+    return [[NavController], [Http]];
   }
 
-  constructor(nav) {
+  constructor(nav, http) {
     this.ext_page = { component: LocationDetailPage };
     this.ext_map_page = { component: MapListPage };
     this.nav = nav;
-    this.items = [
-     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
-     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
-     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
-     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
-     {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."}
-    ];
+    this.http = http;
+    // this.items = [
+    //  {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
+    //  {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
+    //  {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
+    //  {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."},
+    //  {name: "Janique's Place", address: "2151 pacific street", distance: "1.2", avatar: "img/food.jpg", description: "Lorem ipsum dolor sit amet, cibo mundi nam no, hinc fabellas vel in. Pro dicunt admodum at, ad partem feugiat sea. Tale labore ad his, no ludus scripta eos te."}
+    // ];
+    this.items = [];
+    this.loadBusinesses();
   }
 
   itemTapped($event, item){
@@ -30,5 +35,12 @@ export class Page1{
 
   mapView($event){
     this.nav.push(MapListPage)
+  }
+
+  loadBusinesses(){
+    let url = "/api";
+    let response = this.http.get(url).map(res => res.json()).subscribe(data => {
+        this.items = data.businesses;
+    });
   }
 }
