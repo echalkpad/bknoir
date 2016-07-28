3bcb6a09dc9e8d0faace1dc1253507de82d49b75
Anthony Edwards Jr <Anthony.edwardsjr@gmail.com>
2016-07-28 00:09:54 -0400

    added form success popup

--- app/pages/page2/page2.js
+++ app/pages/page2/page2.js
@@ -1,15 +1,19 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController, Alert} from 'ionic-angular';
 import {FORM_DIRECTIVES, FormBuilder,  ControlGroup, Validators, AbstractControl} from '@angular/common';
-
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Page({
   templateUrl: 'build/pages/page2/page2.html'
 })
 export class Page2 {
+
   static get parameters() {
-    return [[FormBuilder]];
+    return [[FormBuilder],[NavController], [Http]];
   }
-  constructor(formBuilder) {
+
+  constructor(formBuilder, nav, http) {
+    this.nav = nav;
 
     this.myForm = formBuilder.group({
       name: [''],
@@ -19,9 +23,25 @@ export class Page2 {
       note: ['']
     });
   }
+  presentAlert(){
+    let alert = Alert.create({
+      title: 'Low battery',
+      subTitle: '10% of battery remaining',
+      buttons: ['Dismiss']
+    });
+    this.nav.present(alert);
+  }
 
   save(){
     console.log(this.myForm.value);
+    let alert = Alert.create({
+      title: 'Thank YOU',
+      subTitle: 'Your listing has been sent to the BKnior Team',
+      buttons: ['Ok']
+    });
+    this.nav.present(alert);
   }
+
+
 }
 
