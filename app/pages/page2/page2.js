37194bd30f183ab12fc381e7e08fae59e739353c
Anthony Edwards Jr <Anthony.edwardsjr@gmail.com>
2016-08-05 02:26:36 -0400

    post http lead form

--- app/pages/page2/page2.js
+++ app/pages/page2/page2.js
@@ -1,6 +1,6 @@
 import {Page, NavController, Alert} from 'ionic-angular';
 import {FORM_DIRECTIVES, FormBuilder,  ControlGroup, Validators, AbstractControl} from '@angular/common';
-import {Http} from '@angular/http';
+import {Http, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Page({
@@ -14,15 +14,16 @@ export class Page2 {
 
   constructor(formBuilder, nav, http) {
     this.nav = nav;
-
+    this.http = http;
     this.myForm = formBuilder.group({
       name: [''],
       address: [''],
       phone: [''],
-      website: [''],
+      web_address: [''],
       note: ['']
     });
   }
+
   presentAlert(){
     let alert = Alert.create({
       title: 'Low battery',
@@ -39,6 +40,16 @@ export class Page2 {
       subTitle: 'Your listing has been sent to the BKnior Team',
       buttons: ['Ok']
     });
+    let body = JSON.stringify(this.myForm.value);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let url = "https://invulnerable-mandarine-47296.herokuapp.com/leads/mobile_create?lead[name]=Test&lead[address]=hommy"
+    let response = this.http.post(url, options).map(res => res.json()).subscribe(data => {
+        console.log(data)
+      },
+      (res) => console.log("res", res),
+      err => console.log('error', err)
+    );
     this.nav.present(alert);
   }
 
